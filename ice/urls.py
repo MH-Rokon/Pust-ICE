47c61062ce554b46bde5bd0f1f8e74df4861692e
MH Rokon <rokonrokon74805@gmail.com>
2024-06-03 18:47:01 +0600

    m

--- ice/urls.py
+++ ice/urls.py
@@ -12,8 +12,4 @@ urlpatterns = [
     path('contacts/', include('contacts.urls')),
     path('notice/', include('notice.urls')),
     path('accounts/', include('accounts.urls')),
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
